f7f8414d267b52283a1c2eaf09a7e6c158f47f32
Hibiki Sakuraoka <wbydo@users.noreply.github.com>
2022-05-21 21:31:59 +0900

    WBYD-119 main.tsのビルド設定追加

---
+++ esbuild.js
@@ -0,0 +1,21 @@
+const { buildSync } = require('esbuild');
+const { resolve } = require('path');
+
+const result = buildSync({
+  entryPoints: [resolve(__dirname, 'src', 'main.ts')],
+  outfile: resolve(__dirname, 'dist', 'main.js'),
+  bundle: true,
+  platform: 'node',
+  target: ['node16'],
+  tsconfig: resolve(__dirname, 'tsconfig.json'),
+});
+
+//正常時のresult -> { errors: [], warnings: [] }
+const { errors, warnings } = result;
+if (errors.length != 0) {
+  console.error(errors);
+}
+
+if (warnings.length != 0) {
+  console.warn(warnings);
+}
